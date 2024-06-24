574f1cdcec5f205b71e0365a5025689bc440cc18
AlonsoDiaz <alo.diaz@duocuc.cl>
2024-06-24 13:23:53 -0400

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,30 @@
+import json
+
+def agregar():
+    with open("biblioteca.json","r") as archivo:
+     contenido = archivo.read()
+ 
+    datos = json.loads(contenido)
+    libros = datos.get("Libro", [])
+
+
+    id_libro = int(input("Ingrese el ID del libro: "))
+    titulo_libro = (input("Ingrese el titulo del libro: "))
+    id_autor = int(input("Ingrese el ID del autor del libro: "))
+    id_categoria = int(input("Ingrese el ID de la categoria del libro: "))
+    publicacion_ano = int(input("Ingrese el año de publicacion del libro: "))
+    isbn = (input("Ingrese el isbn del libro: "))
+
+    agregar = {
+                    "LibroID": id_libro,
+                    "Titulo": titulo_libro,
+                    "AutorID": id_autor,
+                    "CategoriaID": id_categoria,
+                    "AnoPublicacion": publicacion_ano,
+                    "ISBN": isbn
+    }
+
+    libros.append(agregar)
+
+    with open("biblioteca.json","w") as archivo:
+        json.dump(datos,archivo, indent=4)
